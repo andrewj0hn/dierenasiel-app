1df72defd5e9fdbe45484cc61d374398c2c11d85
Andrew <andrew@digitalenterprises.nl>
2018-11-23 20:01:35 +0100

    add featured article to homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import {View, Text} from "react-native";
 import {connect} from "react-redux";
 import Sidebar from "../components/Sidebar";
+import FeaturedArticle from "../components/FeaturedArticle";
+import styles from "./styles";
 
 class Home extends Component {
 	componentDidMount() {
@@ -10,9 +12,10 @@ class Home extends Component {
 
 	render() {
 		return (
-			<View>
+			<View style={styles.container}>
 				<Sidebar/>
 				<Text>Home</Text>
+				<FeaturedArticle/>
 			</View>
 		);
 	}

a32263ba479f508e5074b7f14f6cade58562c0c9
Andrew <andrew@digitalenterprises.nl>
2018-11-23 19:59:18 +0100

    add setup featured article

---
+++ src/components/FeaturedArticle/FeaturedArticle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native";
+import styles from "./styles";
+
+const FeaturedArticle = () => (
+	<View style={styles.container}>
+		<Text>Hoekstra & Partners Notarissen</Text>
+		<Text>Chantal van Hoekstra & Partners Notarissen heeft het initiatief genomen voor een inzamelactie voor het asiel. Cliënten en medewerkers hebben royaal meegedaan aan deze actie. Het prachtige bedrag van € 1.235,00 is overhandigd door Chantal, Lisa, Danielle en Rijnette. We willen Hoekstra & Partners Notarissen heel hartelijk danken voor deze gulle donatie.</Text>
+	</View>
+);
+
+export default FeaturedArticle;

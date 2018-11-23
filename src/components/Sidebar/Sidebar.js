39b57af93a62bf74486782defb2ec2bd9bcfefd7
Andrew <andrew@digitalenterprises.nl>
2018-11-23 20:01:15 +0100

    edit sidebar text to variables

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,20 @@
 import React from "react";
 import {View, Text} from "react-native";
 import styles from "./styles";
+import {
+	SIDEBAR,
+	CATS,
+	DOGS,
+	CONTACT,
+	SETTINGS,
+} from "../../constants/Strings";
 
 export default () => (
 	<View style={styles.container}>
-		<Text>Sidebar</Text>
+		<Text>{SIDEBAR}</Text>
+		<Text>{CATS}</Text>
+		<Text>{DOGS}</Text>
+		<Text>{CONTACT}</Text>
+		<Text>{SETTINGS}</Text>
 	</View>
 )
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -3,6 +3,7 @@ import {StyleSheet} from "react-native";
 export default styles = StyleSheet.create({
 	container: {
 		position: "absolute",
+		zIndex: 1,
 		top: 0,
 		right: 0,
 		bottom: 0,

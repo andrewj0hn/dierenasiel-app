666dac3aaac1d8f7e95b8e20bf04408fe19cfa5a
Andrew John <andrewjohn@Andrews-MacBook-Pro.local>
2018-12-05 16:49:19 +0100

    add statusbar test snapshot

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+// @flow
+import React, { Component } from 'react';
+import { Platform, View, NativeModules } from 'react-native';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import StatusBar from './components/StatusBar/StatusBar';
+import reducers from './reducers';
+import Home from './containers/Home';
+
+const middlewares = compose(applyMiddleware(thunk, logger));
+const store = createStore(reducers, undefined, middlewares);
+const { StatusBarManager } = NativeModules;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusBarHeight: 0,
+    };
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'ios') {
+      StatusBarManager.getHeight((statusBar) => {
+        this.setState({ statusBarHeight: statusBar.height });
+      });
+    }
+  }
+
+  render() {
+    const { statusBarHeight } = this.state;
+    return (
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <StatusBar height={statusBarHeight} />
+          <Home />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
--- src/components/StatusBar/__tests__/StatusBar.test.js
+++ src/components/StatusBar/__tests__/StatusBar.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import App from "./App";
+import StatusBar from "../StatusBar";
 import renderer from "react-test-renderer";
 
 test("renders correctly", () => {
-    const tree = renderer.create(<App/>).toJSON();
+    const tree = renderer.create(<StatusBar/>).toJSON();
     expect(tree).toMatchSnapshot();
 });
